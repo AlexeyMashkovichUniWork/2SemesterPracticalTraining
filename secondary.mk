61eab7b39d519aa439c97e7080b15c476aecf447
AlexeyMashkovichUniWork <231014204+AlexeyMashkovichUniWork@users.noreply.github.com>
2026-05-27 10:35:47 +0300

    fix@make: do not update files if directory updated

--- secondary.mk
+++ secondary.mk
@@ -17,10 +17,10 @@ asm_size: build/$(COMPILER)/size.s
 %_speed: CFLAGS = -O2
 %_size: CFLAGS = -Os
 
-build/$(COMPILER)/exec_%: build/optbench_exec.c build/$(COMPILER)
+build/$(COMPILER)/exec_%: build/optbench_exec.c | build/$(COMPILER)
 	$(COMPILER) $(CFLAGS) -DNO_ZERO_DIVIDE -o $@ $<
 
-build/$(COMPILER)/%.s: $(OPTBENCH) build/$(COMPILER)
+build/$(COMPILER)/%.s: $(OPTBENCH) | build/$(COMPILER)
 	$(COMPILER) -S $(CFLAGS) -o $@ $<
 
 .PHONY: build_all exec_noopt exec_speed exec_size asm_speed asm_size
